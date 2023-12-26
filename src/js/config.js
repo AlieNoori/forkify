e2e6442008fd36ebbf68a81e41889dcfd3b3aad0
alieNoori <alli.nouri.83@gmail.com>
2023-12-26 11:18:31 +0330

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'fbb127b0-cc47-49f3-b2d7-862e6d40a6bc';
+export const MODAL_CLOSE_SEC = 2.5;
